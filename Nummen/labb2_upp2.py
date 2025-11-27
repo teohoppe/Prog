55382efdbfb4a7bc20dd18726d2dc45a49cc21ea
Teo Hoppe <teo.hoppe@gmail.com>
2025-11-27 13:23:32 +0100

    labb2 upp2

--- Nummen/labb2_upp2.py
+++ Nummen/labb2_upp2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.sparse import diags
 
 # a)
 def q(x):
@@ -13,7 +14,7 @@ def diskretisering_temperatur(q,L, k, TL, TR, N, x_approx= 1.0):
     A = np.zeros((N-1, N-1))
     b = np.zeros(N-1)
 
-    coff = k / (h**2)
+    coeff = k / (h**2)
 
     for j in range(N-1):
         A[j, j] = -2
@@ -28,9 +29,9 @@ def diskretisering_temperatur(q,L, k, TL, TR, N, x_approx= 1.0):
     b = b.copy()
     b[0] -= TL
     b[-1] -= TR
-    A *= coff
-    b *= coff
-    
+    A *= coeff
+    b *= coeff
+
     print("Matris A:")
     print(A)
     print("\nHögerled b:")
@@ -49,53 +50,35 @@ def diskretisering_temperatur(q,L, k, TL, TR, N, x_approx= 1.0):
     plt.xlabel("Position längs stången (m)")
     plt.ylabel("Temperatur (°C)")
     plt.grid()
-    plt.plot(x_values, T_values_full, marker='o')
+    plt.plot(x_values, T_values_full, "b-")
     plt.show()
+    
+    return T_approx
 
+# c)
+#diskretisering_temperatur(q, 1, 2, 2, 2, 4, 0.2)
 
-diskretisering_temperatur(q, 1, 2, 2, 2, 4, 0.2)
+# d)
+diskretisering_temperatur(q, 1, 2, 4, 2, 100, 0.2)
 
-diskretisering_temperatur(q, 1, 2, 2, 2, 100, 0.2)
-
+# e)
 def convergence_study(q, L, k, TL, TR, N):
     exakt_sol = 1.6379544
     error = 1.0
-    h_values = []
-    approx_sol = 1.0
+    error_list = []
+    N_list = [] 
 
     while abs(error) > 1e-6:
-        h = L / N
-        h_values.append(h)
-        xi = np.linspace(h, L-h, num=N-1)
-        A = np.zeros((N-1, N-1))
-        b = np.zeros(N-1)
-        coff = k / (h**2)
-
-        for j in range(N-1):
-            A[j, j] = -2
-            if j > 0:
-                A[j, j-1] = 1
-            if j < N-2:
-                A[j, j+1] = 1
-
-        for j in range(1, N):
-            b[j-1] = q(xi[j-1]) * (h**2) / k
-
-        b = b.copy()
-        b[0] -= TL
-        b[-1] -= TR
-        A *= coff
-        b *= coff
-        T_values = np.linalg.solve(A, b)
-        x_values = np.linspace(0, L, N+1)
-        T_values_full = np.zeros(N+1)
-        T_values_full[0] = TL
-        T_values_full[-1] = TR
-        T_values_full[1:-1] = T_values
-        approx_sol = np.interp(0.7, x_values, T_values_full)
-        error = abs(exakt_sol - approx_sol)
+        T_approx = diskretisering_temperatur(q, L, k, TL, TR, N, x_approx=0.7)
+        error = exakt_sol - T_approx
+        error_list.append(error)
+        N_list.append(N)    
+        print(f"N={N}, Approximerad temperatur vid x=0.7: {T_approx:.7f}, Fel: {error:.7e}\n")
         N *= 2
-        
-    print(f"Slutligt N: {N//2}, Approximerad temperatur vid x=0.7: {approx_sol:.7f}, Fel: {error:.7e}")
+        # Nogranhetsordning p
 
-convergence_study(q, 1, 2, 2, 2, 50)
+    if len(error_list) >= 3:
+        p = np.log2(abs(error_list[0] - error_list[1]) / abs(error_list[1] - (error_list[2])))
+        print(f"P går mot {p} ")
+
+#convergence_study(q, 1, 2, 2, 2, 50)
